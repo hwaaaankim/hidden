5686cdbbcc17cb9ddb46c2001f3e7c83e99cf1c0
hwaaaankim <thramania@hanmail.net>
2024-07-12 13:23:48 +0900

    design

--- src/main/resources/static/front/clean/assets/js/detail.js
+++ src/main/resources/static/front/clean/assets/js/detail.js
@@ -7,189 +7,192 @@ let target02 = document.querySelector(".target02");
 let target03 = document.querySelector(".target03");
 let target04 = document.querySelector(".target04");
 
-window.onscroll = function (e) {
+let currentVisibleWrap = null;
+
+window.onscroll = function(e) {
     let tCon = document.getElementById("target-container");
     let height = tCon.getBoundingClientRect().y;
-    console.log(height);
-    if (height > 420) {
-        gsap.to(".wrap01", {
+    let tl = gsap.timeline();
+
+    if (height > 420 && currentVisibleWrap !== "none") {
+        currentVisibleWrap = "none";
+        tl.to(".wrap01", {
             opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap01", { zIndex: 0 });
+            }
         });
-        gsap.to(
-            ".wrap02",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(
-            ".wrap03",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(".wrap04", {
+        tl.to(".wrap02", {
             opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap02", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap03", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap03", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap04", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap04", { zIndex: 0 });
+            }
         });
-    }
-    if (height < 420 && height > -343) {
-        gsap.to(".wrap01", {
+    } else if (height < 420 && height > -343 && currentVisibleWrap !== "wrap01") {
+        currentVisibleWrap = "wrap01";
+        tl.to(".wrap01", {
             opacity: 1,
-            zIndex: 2,
-            duration: 0.75,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap01", { zIndex: 2 });
+            }
         });
-        gsap.to(
-            ".wrap02",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(
-            ".wrap03",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(".wrap04", {
+        tl.to(".wrap02", {
             opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
-        });
-    }
-    if (height < -343 && height > -1343) {
-        gsap.to(".wrap01", {
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap02", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap03", {
             opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
-        });
-        gsap.to(
-            ".wrap02",
-            {
-                opacity: 1,
-                zIndex: 2,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(
-            ".wrap03",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(".wrap04", {
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap03", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap04", {
             opacity: 0,
-            zIndex: 0,
-           duration: 0.75,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap04", { zIndex: 0 });
+            }
         });
-    }
-    if (height < -1343 && height > -2243) {
-        gsap.to(".wrap01", {
+    } else if (height < -343 && height > -1343 && currentVisibleWrap !== "wrap02") {
+        currentVisibleWrap = "wrap02";
+        tl.to(".wrap01", {
             opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap01", { zIndex: 0 });
+            }
         });
-        gsap.to(
-            ".wrap02",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(
-            ".wrap03",
-            {
-                opacity: 1,
-                zIndex: 2,
-               duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(".wrap04", {
-            opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
-        });
-    }
-    if (height < -2243 && height > -3099) {
-        gsap.to(".wrap01", {
-            opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
-        });
-        gsap.to(
-            ".wrap02",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(
-            ".wrap03",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(".wrap04", {
+        tl.to(".wrap02", {
             opacity: 1,
-            zIndex: 2,
-            duration: 0.75,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap02", { zIndex: 2 });
+            }
+        }, "<");
+        tl.to(".wrap03", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap03", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap04", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap04", { zIndex: 0 });
+            }
+        });
+    } else if (height < -1343 && height > -2443 && currentVisibleWrap !== "wrap03") {
+        currentVisibleWrap = "wrap03";
+        tl.to(".wrap01", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap01", { zIndex: 0 });
+            }
+        });
+        tl.to(".wrap02", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap02", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap03", {
+            opacity: 1,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap03", { zIndex: 2 });
+            }
+        }, "<");
+        tl.to(".wrap04", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap04", { zIndex: 0 });
+            }
+        });
+    } else if (height < -2444 && height > -3299 && currentVisibleWrap !== "wrap04") {
+        currentVisibleWrap = "wrap04";
+        tl.to(".wrap01", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap01", { zIndex: 0 });
+            }
+        });
+        tl.to(".wrap02", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap02", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap03", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap03", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap04", {
+            opacity: 1,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap04", { zIndex: 2 });
+            }
+        }, "<");
+    } else if (height < -3300 && currentVisibleWrap !== "none") {
+        currentVisibleWrap = "none";
+        tl.to(".wrap01", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap01", { zIndex: 0 });
+            }
+        });
+        tl.to(".wrap02", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap02", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap03", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap03", { zIndex: 0 });
+            }
+        }, "<");
+        tl.to(".wrap04", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: function() {
+                gsap.set(".wrap04", { zIndex: 0 });
+            }
         });
     }
-    if (height < -3100) {
-        gsap.to(".wrap01", {
-            opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
-        });
-        gsap.to(
-            ".wrap02",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(
-            ".wrap03",
-            {
-                opacity: 0,
-                zIndex: 0,
-                duration: 0.75,
-            },
-            "<"
-        );
-        gsap.to(".wrap04", {
-            opacity: 0,
-            zIndex: 0,
-            duration: 0.75,
-        });
-    }
-  
 };
